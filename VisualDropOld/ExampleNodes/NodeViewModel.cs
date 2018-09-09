f23563e604879e51051f14a1c6989808c9917b97
Christian-Nunnally <christiansnunnally@gmail.com>
2018-09-09 13:12:55 -0500

    Removed visualdrop old because it is not usful any more.

--- VisualDropOld/ExampleNodes/NodeViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using DiagramEditor.ViewModel.Diagram;
-
-namespace ColorOrgan5Nodes.ExampleNodes
-{
-    public class NodeViewModel : PluginNodeViewModel
-    {
-        public override string Name => "Temp Node";
-    }
-}
